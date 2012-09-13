4ad854cceafa1a7b98ad7864d4f26deccf264de0
P. Christeas <xrg@linux.gr>
2012-09-13 12:29:50 +0300

    main menu: put the "setup" menu at the end, include "about" in it
    We want more space in the 1st level of menus.

--- apps/main/__init__.py
+++ apps/main/__init__.py
@@ -12,16 +12,15 @@ from inventory.models import ItemTemplate, Inventory, InventoryTransaction, Supp
 register_menu([
     {'text':_(u'home'), 'view':'home', 'famfam':'house', 'position':0},
 
-    {'text':_(u'setup'), 'view':'location_list', 'links': [
+
+    {'text':_(u'tools'), 'view':'import_wizard', 'links': [
+        {'text':_(u'import'), 'view':'import_wizard', 'famfam':'lightning_add'},
+    ],'famfam':'wrench', 'name':'tools','position':7},
+
+    {'text':_(u'setup'), 'view':'about', 'links': [
         inventory.location_list, assets.state_list,
         movements.purchase_request_state_list,
         movements.purchase_order_state_list,
         movements.purchase_order_item_state_list,
-    ],'famfam':'cog', 'name':'setup','position':7},
-
-    {'text':_(u'tools'), 'view':'import_wizard', 'links': [
-        {'text':_(u'import'), 'view':'import_wizard', 'famfam':'lightning_add'},
-    ],'famfam':'wrench', 'name':'tools','position':6},
-
-    {'text':_(u'about'), 'view':'about', 'position':8},
+    ],'famfam':'cog', 'name':'setup','position':8},
 ])
