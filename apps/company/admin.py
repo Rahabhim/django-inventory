e1abea0d2f5cf829b78037b624a949aad6c7a0e8
P. Christeas <xrg@linux.gr>
2012-09-13 12:28:46 +0300

    company: remove imports of "helpers"
    These utilities were originally developed in a parallel git branch,
    but are not used, since the Django API provides similar functions.

--- apps/company/admin.py
+++ apps/company/admin.py
@@ -1,6 +1,4 @@
-from common import helpers
 from models import DepartmentType, Department
 
-helpers.auto_admin(DepartmentType, Department)
 
 #eof
--- apps/company/urls.py
+++ apps/company/urls.py
@@ -1,6 +1,13 @@
 from models import Department, DepartmentType
-from common.helpers import auto_urls
+from django.conf.urls.defaults import patterns, url, include
+from django.views.generic import ListView
 
-urlpatterns = auto_urls(Department, DepartmentType)
+urlpatterns = patterns('',
+    url(r'^object/list/company_department$', ListView.as_view( model=Department, ),
+            name='company_department_list'),
+    url(r'^object/list/company_department_type$', ListView.as_view( model=DepartmentType,),
+            name='company_department_type_list'),
+    # url(r'^object/list/company_locations$', ListView.as_view()),
+    )
 
 #eof
