ce827eaf2cc456a09c324af223064cf07ceb60fc
P. Christeas <xrg@linux.gr>
2012-09-19 18:25:53 +0300

    products.admin: add more models to admin site

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from models import ItemTemplate
+from models import ItemTemplate, ItemCategory, ItemAttrType, Manufacturer
 
 class ItemTemplateAdmin(admin.ModelAdmin):
     filter_horizontal = ('supplies', 'suppliers')
 
-admin.site.register(ItemTemplate, ItemTemplateAdmin)
+admin.site.register(ItemTemplate, ItemTemplateAdmin,)
+admin.site.register(ItemCategory)
+admin.site.register(ItemAttrType)
+admin.site.register(Manufacturer)
 
 #eof
