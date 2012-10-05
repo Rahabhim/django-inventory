2e5109a2ecf292761e586b58a988ff22fe8ec172
P. Christeas <xrg@linux.gr>
2012-10-05 19:10:50 +0300

    products.models: bundling info in ItemCategory, attributes may be blank
    ItemCategory is the right place to group some bundling information,
    so that the migration process can sort out which asset is the base
    and which are the bundled (contained) ones.

--- apps/products/models.py
+++ apps/products/models.py
@@ -9,6 +9,9 @@ class ItemCategory(models.Model):
     name = models.CharField(max_length=64)
     parent = models.ForeignKey("ItemCategory", related_name="+", blank=True, null=True)
     approved = models.BooleanField(default=False)
+    is_bundle = models.BooleanField(default=False)
+    may_contain = models.ManyToManyField('self', related_name='+', blank=True, null=True,
+            help_text=_(u'Bundles of this category may contain items of these categories'))
 
     @models.permalink
     def get_absolute_url(self):
@@ -31,7 +34,8 @@ class ItemAttrType(models.Model):
                 ('color', 'Color')])
     name = models.CharField(max_length=64, verbose_name=_("name"),
             blank=False)
-    applies_category = models.ManyToManyField(ItemCategory, related_name="applies_cat")
+    applies_category = models.ManyToManyField(ItemCategory, related_name="applies_cat", 
+            blank=True, null=True)
     max_entries = models.IntegerField()
     optional = models.BooleanField()
     in_name = models.BooleanField()
