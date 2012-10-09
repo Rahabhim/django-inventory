1d9ed7e5fe2e3712c798e4db563521b2f2d5111d
P. Christeas <xrg@linux.gr>
2012-10-09 23:17:41 +0300

    company: example of adding a filter on Department.dept_type
    But not generic enough, won't scale to lookup fields etc.

--- apps/company/__init__.py
+++ apps/company/__init__.py
@@ -5,6 +5,10 @@ from django.utils.translation import ugettext_lazy as _
 from common.api import register_links, register_menu
 from common import location_list
 
+import models
+department_type_filter = {'name':'department_type', 'title':_(u'type'), 'queryset': models.DepartmentType.objects.all(), 'destination':'dept_type'}
+
+
 register_menu([
     {'text':_('company'), 'view':'company_department_list', 
             'links':[ {'text':_('departments'), 'view':'company_department_list', 'famfam':'page_go'},
