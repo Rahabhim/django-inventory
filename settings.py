2f4490a003ac45c5b5d2d2267135ddb0ff1dc06c
P. Christeas <xrg@linux.gr>
2012-10-16 10:02:14 +0300

    settings: limit the debug level to "info"
    No need to debug SQL now.

--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ LOGGING = {
     'loggers': {
         'django.db.backends': {
             'handlers': ['console',],
-            'level': 'DEBUG',
+            'level': 'INFO',
             }
         }
     }
@@ -44,13 +44,17 @@ AUTH_URLS = AUTH_BACKEND + '.urls'
 AUTH_PROFILE_MODULE = AUTH_BACKEND+'.UserProfile'
 
 DATABASES = {
-    'default': {
+    'non-default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(PROJECT_ROOT, "%s.sqlite" % PROJECT_NAME),     # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    },
+    'default': {
+        'ENGINE': 'postgresql_psycopg2',
+        'NAME': 'django_inventory',
     }
 }
 
