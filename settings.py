f90b1e7e898940a821eb5c6a0e89185dcae27c5c
P. Christeas <xrg@linux.gr>
2012-11-07 21:50:16 +0200

    settings: activate the carts template context processor

--- settings.py
+++ settings.py
@@ -164,6 +164,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     "django.core.context_processors.media",
     'django.core.context_processors.request',
     'django.core.context_processors.static',
+    'main.cart_utils.in_context',
     "grappelli.context_processors.admin_template_path",
     'django.contrib.messages.context_processors.messages',
 ]
