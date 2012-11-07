d1f9788c5403c1a1d625f49af66b95bb35ce82ee
P. Christeas <xrg@linux.gr>
2012-11-07 21:42:33 +0200

    generic_views: preparation for carts framework
    Carts will be registered in the render context and called from there
    through a CartsContainer object.

--- apps/main/cart_utils.py
+++ apps/main/cart_utils.py
@@ -7,79 +7,91 @@ logger = logging.getLogger('main.carts')
 """
 
     We store scalar information about open "Carts" in the session.
-    
+
     This has the following format
         session['carts'] = list( dict(ref=(app, model, pk), dest=(dapp, dmodel), ),
                     dict(<cart2>), ...)
-            
+
         where:
             app is the application of the Cart object
             model is the model name of the Cart object
             pk the primary key of the opened Cart
-        
+
             dapp, dmodel: application + model of the object this cart shall be filled with
 
+    Then, we place a 'carts' attribute in the RequestContext, through the middleware
+    function `in_context()` . That attribute will hold Model() objects that represent
+    carts.
 """
 
-def get_session_carts(session, init=True, for_model=None):
-    """Get the carts of some session
+class CartsContainer(object):
+    """Holds list of carts, in an indexed way
     """
-    ret = []
-    
-    for_dest =None
-    if for_model is not None:
-        assert issubclass(for_model, models.Model), for_model
+    def __init__(self, session_carts):
+        self._session_carts = session_carts
+        self._carts = {} # holds tuple(<cart_object>, (dest) )
+
+    def __iter__(self):
+        for c, dest in self._carts.values():
+            yield c
+
+    def load_all(self):
+        for scart in self._session_carts:
+            try:
+                sref = scart['ref']
+                cart = models.get_model(sref[0], sref[1]).objects.get(pk=sref[2])
+                self._carts[sref] = (cart, scart.get('dest', None))
+            except ObjectDoesNotExist:
+                logger.debug("Stale cart object in session: %s.%s #%s", *(scart['ref'][0:3]))
+
+    def carts_for_model(self, for_model):
+        assert hasattr(for_model, '_meta'), repr(for_model)
         for_dest = (for_model._meta.app_label, for_model._meta.object_name)
 
-    for scart in session['carts']:
-        if for_dest is not None and scart.get('dest', None) is not None \
-                and scart['dest'] != for_dest:
-            continue
-        try:
-            cart = models.get_model(scart['ref'][0], scart['ref'][1]).objects.get(pk=scart['ref'][2])
-        except ObjectDoesNotExist:
-            logger.debug("Stale cart object in session: %s.%s #%s", *(scart['ref'][0:3]))
-            continue
-        c = dict(title=cart.get_cart_name(), qty=cart.get_cart_itemcount(), \
-                href=cart.get_cart_url())
-        ret.append(c)
-    
-    print "cart ret:", ret
-    return ret
+        for c, dest in self._carts.values():
+            if dest is None or dest == for_dest:
+                yield c
 
-def open_as_cart(obj, session, destination=None):
-    """Register object to be used as a cart within a session
-    """
-    assert isinstance(obj, models.Model), "obj: %s %s" % (type(obj), repr(obj))
-    ref = (obj._meta.app_label, obj._meta.object_name, obj.pk)
-    for c in session['carts']:
-        # scan and avoid duplicates
-        if c.get('ref',None) == ref:
-            break
-    else:
+    def open_as_cart(self, obj, destination=None):
+        """Register object to be used as a cart
+        """
+        assert isinstance(obj, models.Model), "obj: %s %s" % (type(obj), repr(obj))
+        ref = (obj._meta.app_label, obj._meta.object_name, obj.pk)
+        for c in self._session_carts:
+            # scan and avoid duplicates
+            if c.get('ref',None) == ref:
+                return False
+
         if hasattr(destination, '_meta'):
             destination = (destination._meta.app_label, destination._meta.object_name)
         elif isinstance(destination, basestring):
             destination = tuple(destination.split('.', 1))
-        session['carts'].append(dict(ref=ref, dest=destination))
-    session.modified = True
+        self._session_carts.append(dict(ref=ref, dest=destination))
+        self._carts[ref] = (obj, destination)
+        return True
 
-def close_cart(obj, session, destination=None):
-    assert isinstance(obj, models.Model), "obj: %s %s" % (type(obj), repr(obj))
-    ref = (obj._meta.app_label, obj._meta.object_name, obj.pk)
-    new_carts = filter(lambda c: c.get('ref',None) != ref, session['carts'])
-    if len(new_carts) < len(session['carts']):
-        session['carts'] = new_carts
-        session.modified = True
+    def close_cart(self, obj):
+        assert isinstance(obj, models.Model), "obj: %s %s" % (type(obj), repr(obj))
+        ref = (obj._meta.app_label, obj._meta.object_name, obj.pk)
 
-def needed_in_cart(obj, session):
-    """See if object can be added to any of session's carts
-    """
-    fdest = (obj._meta.app_label, obj._meta.object_name)
-    for cart in session.get('carts', []):
-        cdest = cart.get('dest', None)
-        if cdest == fdest or cdest is None:
-            return True
-    return False
+        modified = False
+        to_del = []
+        for scart in self._session_carts:
+            if scart.get('ref') == ref:
+                to_del.append(scart)
+
+        for td in to_del:
+            # not optimal, but safe...
+            self._session_carts.remove(td)
+            modified = True
+
+        self._carts.pop(ref, None)
+        return modified
+
+def in_context(request):
+    if 'carts' not in request.session:
+        request.session['carts'] = [] # these are in non-object form
+
+    return dict(carts=CartsContainer(request.session['carts'])) # but these will receive active objects
 
 #eof
--- apps/main/templatetags/carts.py
+++ apps/main/templatetags/carts.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from django import template
 import logging
-from main import cart_utils
+# from main import cart_utils
 from django.db import models
 import settings
 
@@ -11,16 +11,21 @@ logger = logging.getLogger('main.carts')
 @register.simple_tag(takes_context=True)
 def prepare_session_carts(context):
     try:
-        session = template.Variable('request').resolve(context).session
-        if 'carts' not in session:
-            session['carts'] = []
-            # session.modified = True
+        logger.warning("Prepare session carts for context %x", id(context))
+        context['carts'].load_all()
+
     except Exception:
-        logger.warning("Cannot resolve session:", exc_info=True)
+        logger.warning("Cannot prepare carts:", exc_info=True)
         if settings.DEBUG:
             raise
-    
+
     return ''
+
+def cart2dict(cart):
+    """Formalize the dictionary of a cart passed for rendering
+    """
+    return dict(title=cart.get_cart_name(), qty=cart.get_cart_itemcount(), \
+                href=cart.get_cart_url(), cart_id=hex(id(cart))[-7:-1])
 
 @register.inclusion_tag('session_carts.html', takes_context=True)
 def session_carts(context):
@@ -29,12 +34,10 @@ def session_carts(context):
         They should appear at the top of the menu or so.
         Also calls "prepare_session_carts"
     """
-    session = None
     prepare_session_carts(context)
-
     try:
-        session = template.Variable('request').resolve(context).session
-        return {'carts': cart_utils.get_session_carts(session) }
+        rcarts = context['carts']
+        return {'carts': map(cart2dict, rcarts) }
 
     except Exception:
         logger.warning("Cannot resolve session carts:", exc_info=True)
@@ -47,15 +50,26 @@ def session_carts(context):
 def object_carts(context, obj):
     """ Carts to appear as "actions" inside some object's line
     """
-    ret = {}
+    ret = {'object': obj, 'carts': []}
+
     try:
-        session = template.Variable('request').resolve(context).session
-        if session is not None and obj is not None and isinstance(obj, models.Model)\
-                and cart_utils.needed_in_cart(obj, session):
-            ret['has_cart'] = True
+        if obj is not None and isinstance(obj, models.Model):
+            for cart in context['carts'].carts_for_model(obj):
+                c = cart2dict(cart)
+                cstate, href = cart.get_cart_objcap(obj)
+                if cstate:
+                    c['item_state'] = cstate
+                    c['action_url'] = href + ('?item=%d' % obj.id)
+                ret['carts'].append(c)
+
+        ret['has_cart'] = bool(ret['carts'])
+
+            # jQuery cart
     except Exception:
-        logger.warning("Cannot resolve session carts:", exc_info=True)
-    
+        logger.warning("Cannot resolve object carts:", exc_info=True)
+        if settings.DEBUG:
+            raise
+
     return ret
 
 @register.inclusion_tag('object_list_carts.html', takes_context=True)
@@ -64,13 +78,25 @@ def object_list_carts(context, queryset):
     """
     ret = {}
     try:
-        session = template.Variable('request').resolve(context).session
-        if session is not None and queryset is not None:
-            ret['carts'] = cart_utils.get_session_carts(session, init=False, for_model=queryset.model)
+        if queryset is not None and hasattr(queryset, 'model'):
+            ret['carts'] = map(cart2dict, context['carts'].carts_for_model(queryset.model))
             ret['has_cart'] = bool(ret['carts'])
     except Exception:
         logger.warning("Cannot resolve session carts:", exc_info=True)
-    
+        if settings.DEBUG:
+            raise
+    return ret
+
+@register.simple_tag(takes_context=True)
+def do_action(context):
+    ret = ''
+    try:
+        ret = context['action_fn'](context) or ''
+        context['action_done'] = True
+    except Exception:
+        logger.warning("Cannot do action:", exc_info=True)
+        if settings.DEBUG:
+            raise
     return ret
 
 
