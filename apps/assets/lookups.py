c96fed42695c5105e18d711d6d6ce6e6a4356cbc
P. Christeas <xrg@linux.gr>
2012-11-22 10:45:09 +0200

    assets/lookups: match Items by serial or property numbers
    If either of those numbers (or codes) are given, select that Item
    immediately.

--- apps/assets/lookups.py
+++ apps/assets/lookups.py
@@ -11,5 +11,18 @@ from common.api import LookupChannel
 class ItemLookup(LookupChannel):
     model = Item
     search_field = 'item_template__description' ## FIXME: use serial, too...
+    def get_query(self, q, request):
+        if not request.user.is_authenticated():
+            raise HttpResponseForbidden()
+
+        cur = self.model.objects
+        filt = models.Q(property_number=q)|models.Q(serial_number=q)
+        if cur.filter(filt).exists():
+            # only those who match exact the property/serial
+            return cur.filter(filt)
+
+        for r in q.split(' '):
+            cur = cur.filter(**{"%s__icontains" % self.search_field: r})
+        return cur[:self.max_length]
 
 #eof
