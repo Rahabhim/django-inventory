8d1dac5ddc57cab4bfbdfa62eed9356e52b00bc2
P. Christeas <xrg@linux.gr>
2012-11-26 19:26:47 +0200

    assets.forms: most fields are read-only. Only edit the serial + notes

--- apps/assets/forms.py
+++ apps/assets/forms.py
@@ -2,19 +2,23 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-from generic_views.forms import DetailForm, ColumnsDetailWidget, DetailForeignWidget
+from generic_views.forms import DetailForm, ColumnsDetailWidget, DetailForeignWidget, ReadOnlyInput
 
 from models import Item, ItemGroup
 from common.models import Location
+from products.models import ItemTemplate
 from ajax_select.fields import AutoCompleteSelectField
 
 class ItemForm(forms.ModelForm):
-    item_template = AutoCompleteSelectField('product', label=_("product"), show_help_text=False)
+    #item_template = AutoCompleteSelectField('product', label=_("product"), show_help_text=False)
     # location = AutoCompleteSelectField('location', label=_("current location"), required=False, show_help_text=False)
     class Meta:
         model = Item
         exclude = ('photos', 'active', 'is_bundled', 'qty')
-        widgets = {'location': DetailForeignWidget(queryset=Location.objects.all()) }
+        widgets = {'location': DetailForeignWidget(queryset=Location.objects.all()),
+                    'item_template': DetailForeignWidget(queryset=ItemTemplate.objects.all()),
+                    'property_number': ReadOnlyInput,
+                    }
 
 class ItemForm_view(DetailForm):
     class Meta:
@@ -25,7 +29,10 @@ class ItemGroupForm(ItemForm):
     class Meta:
         model = ItemGroup
         exclude = ('items', 'active', 'is_bundled', 'qty')
-        widgets = {'location': DetailForeignWidget }
+        widgets = {'location': DetailForeignWidget(queryset=Location.objects.all()),
+                    'item_template': DetailForeignWidget(queryset=ItemTemplate.objects.all()),
+                    'property_number': ReadOnlyInput,
+                    }
 
 class SubItemsDetailWidget(ColumnsDetailWidget):
     columns = [{'name': _(u'Contained Item')}, 
@@ -43,8 +50,11 @@ class ItemGroupForm_edit(ItemForm):
     class Meta:
         model = ItemGroup
         exclude = ('photos', 'active', 'is_bundled', 'qty')
-        widgets = {'items': SubItemsDetailWidget, \
-                'location': DetailForeignWidget(queryset=Location.objects.all()) }
+        widgets = {'items': SubItemsDetailWidget,
+                'location': DetailForeignWidget(queryset=Location.objects.all()),
+                'item_template': DetailForeignWidget(queryset=ItemTemplate.objects.all()),
+                    'property_number': ReadOnlyInput,
+                }
                 # read-only widget, edits must be done through special actions.
 
 #eof
