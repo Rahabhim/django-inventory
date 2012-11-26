673d42617ce025a2b7ea28bf7424697f47aac942
P. Christeas <xrg@linux.gr>
2012-11-26 19:24:19 +0200

    assets.item: remove create/update/delete items from menu
    These shall not be executed directly, but rather computed through
    movements.

--- apps/assets/__init__.py
+++ apps/assets/__init__.py
@@ -13,28 +13,30 @@ state_delete = {'text':_(u'delete asset state'), 'view':'state_delete', 'args':'
 
 
 asset_list = {'text':_('list all assets'), 'view':'item_list', 'famfam':'computer'}
-asset_create = {'text':_('create new asset'), 'view':'item_create', 'famfam':'computer_add'}
-asset_edit = {'text':_(u'edit'), 'view':'item_update', 'args':'object.id', 'famfam':'computer_edit'}
-asset_delete = {'text':_(u'delete'), 'view':'item_delete', 'args':'object.id', 'famfam':'computer_delete'}
+# asset_create = {'text':_('create new asset'), 'view':'item_create', 'famfam':'computer_add'}
+asset_edit = {'text':_(u'edit serials'), 'view':'item_update', 'args':'object.id', 'famfam':'computer_edit'}
+# asset_delete = {'text':_(u'delete'), 'view':'item_delete', 'args':'object.id', 'famfam':'computer_delete'}
 asset_photos = {'text':_(u'add / remove photos'), 'view':'item_photos', 'args':'object.id', 'famfam':'picture_edit'}
 asset_template = {'text':_(u'template'), 'view':'template_view', 'args':'object.item_template.id', 'famfam':'page_go'}
 
+
 group_list = {'text':_(u'list all groups'), 'view':'group_list', 'famfam':'chart_pie'}
-group_create = {'text':_(u'create group'), 'view':'group_create', 'famfam':'chart_pie_add'}
-group_update = {'text':_(u'edit'), 'view':'group_update', 'args':'object.id', 'famfam':'chart_pie_edit'}
-group_delete = {'text' : _(u'delete'), 'view':'group_delete', 'args':'object.id', 'famfam':'chart_pie_delete'}
+group_edit = {'text':_(u'edit serials'), 'view':'group_update', 'args':'object.item_ptr.id', 'famfam':'computer_edit'}
+
+#group_create = {'text':_(u'create group'), 'view':'group_create', 'famfam':'chart_pie_add'}
+#group_update = {'text':_(u'edit'), 'view':'group_update', 'args':'object.id', 'famfam':'chart_pie_edit'}
+#group_delete = {'text' : _(u'delete'), 'view':'group_delete', 'args':'object.id', 'famfam':'chart_pie_delete'}
 
 state_filter = {'name':'state', 'title':_(u'state'), 'queryset':State.objects.all(), 'destination':'itemstate'}
 
 
-register_links(['item_list', 'item_view', 'item_create', # 'item_orphans_list', 
-        'item_update', 'item_delete', 'item_photos', # 'item_assign_person', 
-        'template_items_list'], [asset_create], menu_name='sidebar')
+#register_links(['item_list', 'item_view',
+        #'item_update_serials', 'item_delete', 'item_photos',
+        #'template_items_list'], [asset_create], menu_name='sidebar')
 
-register_links(Item, [asset_edit, asset_delete, asset_template], menu_name='sidebar')
+register_links(Item, [asset_edit, asset_template], menu_name='sidebar')
 
-register_links(['group_list', 'group_view', 'group_create', 'group_update', 'group_delete'], [group_create], menu_name='sidebar')
-register_links(ItemGroup, [group_update, group_delete])
+register_links(ItemGroup, [group_edit, asset_template])
 
 register_links(['state_list', 'state_create', 'state_update', 'state_delete'], [state_create], menu_name='sidebar')
 register_links(State, [state_edit, state_delete])
