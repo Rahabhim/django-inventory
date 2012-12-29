cabcc83f5599ec3e9a7ec9b75010873ca7bd5cf8
P. Christeas <xrg@linux.gr>
2012-12-29 18:15:00 +0200

    movements: PO Wizard step 2, stubs for steps 3-5
    Step 2 is the multiple selection of Product Categories. Using the
    special widget of previous commit, lay the categories in the tabbed
    matrix and let the user select them.

--- apps/movements/views_po_wizard.py
+++ apps/movements/views_po_wizard.py
@@ -5,25 +5,39 @@ from django import forms
 from django.shortcuts import render_to_response
 from django.contrib.formtools.wizard.views import SessionWizardView
 
+from products.models import ItemCategory
+from products.forms import CategoriesMultiSelectWidget
+from procurements.models import Contract
+
 class PO_Step1(forms.Form):
+    title = _("Purchase Order Header Data")
     user_id = forms.CharField(max_length=32, required=False, label=_(u'purchase order number'))
     #procurement = models.ForeignKey('procurements.Contract', null=True, blank=True, label=_("procurement contract"))
-    #supplier = models.ForeignKey(Supplier, label=_(u'supplier'))
-    issue_date = forms.DateField(label=_(u'issue date'))
-    vat_number = forms.CharField(max_length=12, label=_("VAT number"))
+    issue_date = forms.DateField(label=_(u'issue date'), required=False)
+    
+    contract = forms.ModelChoiceField(label=_("Procurement Contract"), queryset=Contract.objects.all())
+    name_or_vat = forms.ChoiceField(label=_('Find by'), widget=forms.widgets.RadioSelect,
+            choices=[('vat', _('VAT (exact)')), ('name', _('Company Name'))], )
+    name_supplier = forms.CharField(max_length=12, label=_('Company Name'), required=False)
+    vat_supplier = forms.CharField(max_length=12, label=_("VAT number"), required=False)
 
 class PO_Step2(forms.Form):
-    #categories = forms.SomeField()
-    pass
+    title = _("Select Product Categories")
+    categories = forms.ModelMultipleChoiceField(queryset=ItemCategory.objects.filter(approved=True),
+            widget=CategoriesMultiSelectWidget)
+
 
 class PO_Step3(forms.Form):
-    subject = forms.CharField(max_length=100)
+    title = _("Input Product Details")
+    subject = forms.CharField(max_length=100, required=False)
 
 class PO_Step4(forms.Form):
-    subject = forms.CharField(max_length=100)
+    title = _("Final Review")
+    subject = forms.CharField(max_length=100, required=False)
 
 class PO_Step5(forms.Form):
-    subject = forms.CharField(max_length=100)
+    title = _("Successful Entry - Finish")
+    subject = forms.CharField(max_length=100, required=False)
 
 
 class PO_Wizard(SessionWizardView):
@@ -39,18 +53,16 @@ class PO_Wizard(SessionWizardView):
         return super(PO_Wizard,cls).as_view(cls.form_list, **kwargs)
 
     def get_template_names(self):
-        print "current: %r" % self.steps.current
         return ['po_wizard_step%s.html' % self.steps.current,]
-        
+
     def get_context_data(self, form, **kwargs):
         context = super(PO_Wizard, self).get_context_data(form, **kwargs)
-        context.update(wiz_steps=[('1', u'Εισαγωγή στοιχείων Τιμολογίου Εξοπλισμού'), ('2', 'Επιλογή είδους Εξοπλισμού'), ('3', u'Καταχώρηση στοιχείων Εξοπλισμού'), ('4', u'Τελικός έλεγχος'), ('5', u'Επιτυχής καταχώρηση - τέλος') ],
-            wiz_width=20)
+        context.update(wiz_steps=self.form_list.items(),
+            wiz_width=(100/len(self.form_list)))
         return context
 
 def get_po_wizview(*args, **kwargs):
     return PO_Wizard.as_view(**kwargs)
 
-#TemplateView.as_view(template_name="po_wizard_step1.html",
-#            extra_context=dict()
+
 #eof
