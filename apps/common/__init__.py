4c39ac7db8a9e82f65ed0ecdb3e6e6779067f1be
P. Christeas <xrg@linux.gr>
2013-01-16 09:57:34 +0200

    common: protect Supplier, Location's actions from anauthorized users

--- apps/common/__init__.py
+++ apps/common/__init__.py
@@ -3,21 +3,28 @@
 # Only a few rights reserved
 
 from django.utils.translation import ugettext_lazy as _
-from common.api import register_links, register_menu
+from common.api import register_links, register_menu, can_add, can_edit, can_delete
 
 from models import Supplier, Location
 
 supplier_list = {'text':_('suppliers'), 'view':'supplier_list', 'famfam':'lorry'}
-supplier_create = {'text':_('create new supplier'), 'view':'supplier_create', 'famfam':'lorry_add'}
-supplier_update = {'text':_('edit'), 'view':'supplier_update', 'args':'object.id', 'famfam':'lorry'}
-supplier_delete = {'text':_('delete'), 'view':'supplier_delete', 'args':'object.id', 'famfam':'lorry_delete'}
-supplier_assign_itemtemplate = {'text':_(u'assign templates'), 'view':'supplier_assign_itemtemplates', 'args':'object.id', 'famfam':'page_go'}
+supplier_create = {'text':_('create new supplier'), 'view':'supplier_create', 'famfam':'lorry_add',
+            'condition': can_add(Supplier)}
+supplier_update = {'text':_('edit'), 'view':'supplier_update', 'args':'object.id', 'famfam':'lorry',
+            'condition': can_edit}
+supplier_delete = {'text':_('delete'), 'view':'supplier_delete', 'args':'object.id', 'famfam':'lorry_delete',
+            'condition': can_delete}
+supplier_assign_itemtemplate = {'text':_(u'assign templates'), 'view':'supplier_assign_itemtemplates', 'args':'object.id', 'famfam':'page_go',
+            'condition': can_edit}
 supplier_purchase_orders = {'text':_(u'related purchase orders'), 'view':'supplier_purchase_orders', 'args':'object.id', 'famfam':'cart_go'}
 
 location_list = {'text':_('locations'), 'view':'location_list', 'famfam':'map'}
-location_create = {'text':_(u'create new location'), 'view':'location_create', 'famfam':'map_add'}
-location_update = {'text':_(u'edit'), 'view':'location_update', 'args':'object.id', 'famfam':'map_edit'}
-location_delete = {'text':_(u'delete'), 'view':'location_delete', 'args':'object.id', 'famfam':'map_delete'}
+location_create = {'text':_(u'create new location'), 'view':'location_create', 'famfam':'map_add',
+            'condition': can_add(Location) }
+location_update = {'text':_(u'edit'), 'view':'location_update', 'args':'object.id', 'famfam':'map_edit',
+            'condition': can_edit}
+location_delete = {'text':_(u'delete'), 'view':'location_delete', 'args':'object.id', 'famfam':'map_delete',
+            'condition': can_delete}
 location_assets = {'text':_(u'assets'), 'view':'location_assets', 'args':'object.id', 'famfam':'computer'}
 
 
