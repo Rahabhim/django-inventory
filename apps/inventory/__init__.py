b6beec2ce8442e7119f01a190387977536e0aff0
P. Christeas <xrg@linux.gr>
2013-01-22 11:25:10 +0200

    inventory: cleanup unused actions from init

--- apps/inventory/__init__.py
+++ apps/inventory/__init__.py
@@ -19,11 +19,6 @@ inventory_open = {'text':_(u'open'), 'view':'inventory_open', 'args':'object.id'
 inventory_close = {'text':_(u'close'), 'view':'inventory_close', 'args':'object.id', 'famfam':'package_red'}
 inventory_compare = {'text': _(u'compare'), 'view':'inventory_items_compare', 'args':'object.id', 'famfam':'package_go',  'condition': lambda o,c: not o.date_val}
 
-#inventory_transaction_list = {'text':_('view all transactions'), 'view':'inventory_transaction_list', 'famfam':'book_go'}
-#inventory_transaction_create = {'text':_('create new transaction'), 'view':'inventory_transaction_create', 'famfam':'book_add'}
-#inventory_transaction_update = {'text':_(u'edit'), 'view':'inventory_transaction_update', 'args':'object.id', 'famfam':'book_add'}
-#inventory_transaction_delete = {'text':_(u'delete'), 'view':'inventory_transaction_delete', 'args':'object.id', 'famfam':'book_delete'}
-#inventory_transaction_view = {'text':_(u'details'), 'view':'inventory_transaction_view', 'args':'object.id', 'famfam':'book_go'}
 
 jump_to_template = {'text':_(u'template'), 'view':'template_view', 'args':'object.supply.id', 'famfam':'page_go'}
 jump_to_inventory = {'text':_(u'return to inventory'), 'view':'inventory_view', 'args':'object.inventory.id', 'famfam':'package_go'}
@@ -38,10 +33,6 @@ register_links(['inventory_view', 'inventory_list', 'inventory_create',
 register_links(Inventory, [inventory_update, inventory_compare])
 register_links(Inventory, [inventory_delete, inventory_open, inventory_close], menu_name='sidebar')
 register_links(Inventory, [inventory_view], menu_name='sidebar')
-
-#register_links(['inventory_transaction_list', 'inventory_transaction_create', 'inventory_transaction_update', 'inventory_transaction_delete', 'inventory_transaction_view'], [inventory_create_transaction], menu_name='sidebar')
-#register_links(InventoryTransaction, [inventory_transaction_view, inventory_transaction_update, inventory_transaction_delete, jump_to_template])
-#register_links(InventoryTransaction, [jump_to_inventory], menu_name='sidebar')
 
 
 def has_pending_inventories(obj, context):
