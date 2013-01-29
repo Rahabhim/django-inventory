b05dd646184ccbefb4d32fecd44ca62ff6aa1494
P. Christeas <xrg@linux.gr>
2013-01-29 10:12:57 +0200

    products/form_fields: Categories Attributes widget
    So, a product (aka. ItemTemplate) may have some set attributes, according
    to its category. We want this to appear at a custom widget, where each
    attribute will be a combo of possible values. This is a bit awkward to
    the MVC structure of Django, since the combos and values depend on the
    category of the product.
    
    In implementation terms, the Field+Widget expect a dict as `initial`
    value, which will contain a 'from_category' item. Then, all combos will
    have the same `name` in html, so that all selections are sent together
    by the browser, in a list.

--- apps/products/form_fields.py
+++ apps/products/form_fields.py
@@ -115,5 +115,168 @@ class CategoriesSelectWidget(forms.widgets.Select):
             self._renderer = CSWRenderer(self.queryset, name, value, final_attrs)
         return self._renderer
 
+class CATItem(StrAndUnicode):
+    """Product Attribute selection
+
+        This will display a combo box with values for one Attribute applicable
+        to some product of a specific category.
+    """
+
+    def __init__(self, parent_name, cattr, values):
+        """
+            @param parent the widget
+            @param cattr the category attribute
+        """
+        self._parent_name = parent_name
+        self._label_name = cattr.name
+        self._attr_id = cattr.id
+        self._is_required = cattr.required
+        self._values = []
+        for v in cattr.values.all():
+            self._values.append((v.id, v.value, bool(v.id in values)))
+
+    def _make_label(self):
+        return u'<label for="%s">%s</label>'  % (self.did, self._label_name)
+
+    def __unicode__(self):
+        """returns the full option element
+        """
+        return self._make_label() + self.option
+
+    @property
+    def did(self):
+        """div-id of this selection element
+        """
+        return mark_safe("%s-a%d" %(self._parent_name, self._attr_id))
+
+    @property
+    def name(self):
+        """ID of the parent field
+        """
+        return self._parent_name
+
+    @property
+    def label(self):
+        """ The label to display by the option
+        """
+        return mark_safe(self._make_label())
+
+    @property
+    def option(self):
+        ret = []
+        ret.append(u'<select id="%s" name="%s">' % (self.did, self._parent_name))
+        ret.append(u'<option value="">-------</option>')
+        for vid, label, selected in self._values:
+            ret.append(u'<option value="%d"%s>%s</option>' % (
+                    vid, selected and ' selected="selected"' or '',
+                    conditional_escape(force_unicode(label)) ))
+        ret.append(u'</select>')
+        return u''.join(ret)
+
+class CategoriesAttributesWidget(forms.widgets.Widget):
+    def __init__(self, queryset=None, choices=(), *args, **kwargs):
+        super(CategoriesAttributesWidget, self).__init__(*args, **kwargs)
+        self.queryset = queryset
+        self.choices = choices # but don't render them to list
+
+    def subwidgets(self, name, value, attrs=None, choices=()):
+        if not (value and ('from_category' in value)):
+            raise KeyError("Value is not prepared!")
+        for cattr in value['from_category'].attributes.all():
+            yield CATItem(name, cattr, value.get('all', []))
+
+    def render(self, name, value, attrs=None, choices=()):
+        final_attrs = self.build_attrs(attrs, name=name)
+        
+        ret = []
+        ret.append(u'<div%s>' % flatatt(final_attrs))
+        for sw in self.subwidgets(name, value, attrs):
+            ret.append( unicode(sw))
+        ret.append('</div>')
+        return mark_safe(u''.join(ret))
+
+
+    def value_from_datadict(self, data, files, name):
+        """
+        Given a dictionary of data and this widget's name, returns the value
+        of this widget. Returns None if it's not provided.
+        """
+        if isinstance(data, (MultiValueDict, MergeDict)):
+            return data.getlist(name)
+        return data.get(name, None)
+
+    def _has_changed(self, initial, data): # TODO
+        print "has_changed", initial, data
+        if initial is None:
+            initial = []
+        if data is None:
+            data = []
+        if len(initial) != len(data):
+            return True
+        initial_set = set([force_unicode(value) for value in initial])
+        data_set = set([force_unicode(value) for value in data])
+        return data_set != initial_set
+
+
+class CategoriesAttributesField(forms.Field):
+    widget = CategoriesAttributesWidget
+    default_error_messages = {
+        'invalid_category': _("Attribute %(attribute)s does not apply to category %(category)s"),
+        'duplicate_attribute': _("Attribute %(attribute)s specified twice"),
+        'missing_attribute': _("Attribute %(attribute)s is required for %(category)s"),
+        'missing_attributes': _("Attributes %(attributes)s are required for %(category)s"),
+        }
+
+    def to_python(self, value):
+        return filter(bool, value)
+
+    def bound_data(self, data, initial):
+        ret = initial.copy()
+        if data:
+            ret['all'] = data
+        return ret
+
+    def validate(self, value):
+        return super(CategoriesAttributesField, self).validate(value)
+
+    def post_clean(self, instance, cleaned_data):
+        """ Cleanup of 'attributes' in cleaned_data, according to `instance.category`
+        
+            We have to obey the rules of attributes for the item's category, ie.
+            force those attributes required, reject ones that do not apply to the
+            category
+        """
+        
+        orig_aval_ids = map(int, cleaned_data.pop('attributes', []))
+        attribs = {}
+        for id, required, name in instance.category.attributes.values_list('id', 'required', 'name'):
+            attribs[id] = {'name': name, 'required': required }
+        clean_attribs = []
+
+        for aval in ProductAttributeValue.objects.filter(id__in=orig_aval_ids):
+            if aval.atype.applies_category_id != instance.category_id:
+                raise forms.ValidationError(self.error_messages['invalid_category'] % \
+                        {'attribute': aval.atype.name, 'category': unicode(instance.category)})
+            found = attribs.pop(aval.atype_id, None)
+            if not found:
+                raise forms.ValidationError(self.error_messages['duplicate_attribute'] % \
+                        {'attribute': aval.atype.name})
+            clean_attribs.append(aval.id)
+        
+        for key in attribs.keys():
+            if not attribs[key]['required']:
+                attribs.pop(key)
+
+        if attribs:
+            if len(attribs) > 1:
+                raise forms.ValidationError(self.error_messages['missing_attributes'] % \
+                            { 'category': unicode(instance.category),
+                                'attributes': ', '.join([a['name'] for a in attribs.values()]) })
+            else:
+                raise forms.ValidationError(self.error_messages['missing_attribute'] % \
+                            { 'category': unicode(instance.category),
+                                'attribute': attribs.values()[0]['name'] })
+        
+        cleaned_data['attributes'] = clean_attribs
 
 #eof
