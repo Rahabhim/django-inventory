fcc3904f7c419d8f70544ba94208ae4f8de96b87
P. Christeas <xrg@linux.gr>
2013-02-05 12:40:24 +0200

    movements: intermediate PO wizard steps 2,3
    WIP alert

--- apps/movements/views_po_wizard.py
+++ apps/movements/views_po_wizard.py
@@ -5,9 +5,11 @@ from django import forms
 from django.shortcuts import render_to_response
 from django.contrib.formtools.wizard.views import SessionWizardView
 
-from products.models import ItemCategory
-from products.forms import CategoriesMultiSelectWidget
+from products.models import ItemCategory, Manufacturer
+from products.form_fields import CategoriesSelectWidget, CategoriesAttributesField
 from procurements.models import Contract
+from ajax_select.fields import AutoCompleteSelectField, AutoCompleteSelectMultipleField
+from django.db.models import Count
 
 class PO_Step1(forms.Form):
     title = _("Purchase Order Header Data")
@@ -17,19 +19,36 @@ class PO_Step1(forms.Form):
     
     contract = forms.ModelChoiceField(label=_("Procurement Contract"), queryset=Contract.objects.all())
     name_or_vat = forms.ChoiceField(label=_('Find by'), widget=forms.widgets.RadioSelect,
+            initial='name',
             choices=[('vat', _('VAT (exact)')), ('name', _('Company Name'))], )
-    name_supplier = forms.CharField(max_length=12, label=_('Company Name'), required=False)
-    vat_supplier = forms.CharField(max_length=12, label=_("VAT number"), required=False)
+    name_supplier = AutoCompleteSelectField('supplier_name', label=_("Company Name"), required=False)
+    vat_supplier = AutoCompleteSelectField('supplier_vat', label=_("VAT number"), required=False)
 
 class PO_Step2(forms.Form):
     title = _("Select Product Categories")
-    categories = forms.ModelMultipleChoiceField(queryset=ItemCategory.objects.filter(approved=True),
-            widget=CategoriesMultiSelectWidget)
+    new_category = forms.ModelChoiceField(queryset=ItemCategory.objects.filter(approved=True),
+            widget=CategoriesSelectWidget)
 
 
 class PO_Step3(forms.Form):
     title = _("Input Product Details")
-    subject = forms.CharField(max_length=100, required=False)
+    item_template = AutoCompleteSelectField('product', label=_("Product"), show_help_text=False, required=False)
+    product_number = forms.CharField(max_length=100, required=False, label=_("Product number"))
+    quantity = forms.IntegerField(label=_("Quantity"), initial=1)
+    serials = forms.CharField(label=_("Serial numbers"), required=False, widget=forms.widgets.Textarea)
+    manufacturer = forms.ModelChoiceField(label=_("manufacturer"), queryset=Manufacturer.objects.none(), required=False)
+    product_attributes = CategoriesAttributesField(label=_("attributes"), required=False)
+    item_template2 = forms.ChoiceField(label=_("Product"), choices=(('','------'),), required=False)
+
+    def __init__(self, data=None, files=None, **kwargs):
+        super(PO_Step3, self).__init__(data, files, **kwargs)
+        if 'product_attributes' in self.initial and 'from_category' in self.initial['product_attributes']:
+            # Set the manufacturer according to the existing products of `from_category` ,
+            # in descending popularity order
+            self.fields['manufacturer'].queryset = Manufacturer.objects.\
+                        filter(products__category=self.initial['product_attributes']['from_category']).\
+                        annotate(num_products=Count('products')).order_by('-num_products')
+        # self.initial['product_attributes']['all'] = []
 
 class PO_Step4(forms.Form):
     title = _("Final Review")
@@ -60,6 +79,13 @@ class PO_Wizard(SessionWizardView):
         context.update(wiz_steps=self.form_list.items(),
             wiz_width=(100/len(self.form_list)))
         return context
+    
+    def get_form_initial(self, step):
+        ret = super(PO_Wizard, self).get_form_initial(step)
+        if step == '3':
+            step2_data =  self.get_cleaned_data_for_step('2')
+            ret['product_attributes'] = {'from_category': step2_data.get('new_category', None), 'all': [] }
+        return ret
 
 def get_po_wizview(*args, **kwargs):
     return PO_Wizard.as_view(**kwargs)
