b6ec2f504e99db8a8dbea7eb96fdcf6fcd9f2331
P. Christeas <xrg@linux.gr>
2013-02-18 12:04:19 +0200

    home, auth_passwd: AJAX/JSON hook for select user role
    This is just inactive code. The user role could be done asynchronously,
    but then the home page needs to be refreshed anyway.

--- apps/auth_passwd/views.py
+++ apps/auth_passwd/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, get_object_or_404
 from django.utils.translation import ugettext as _
 from django.contrib import messages
+from django.utils import simplejson
+from django.http import HttpResponse
 
 from models import DepartmentRole
 
@@ -9,13 +11,19 @@ def password_change_done(request):
     return redirect('home')
 
 def select_user_role(request):
-    role_id = request.GET.get('role_id', None)
+    role_id = request.REQUEST.get('role_id', None)
+    http_accept = request.META.get('HTTP_ACCEPT','-')
     if role_id:
         role = get_object_or_404(DepartmentRole, pk=role_id)
         assert role.user == request.user, "User mismatch!"
 
         request.session['current_user_role'] = role.id
 
-    return redirect('home')
+    if request.is_ajax() and ('application/json' in http_accept):
+        return_dict = {'success': True, 'data': role.id}
+        json = simplejson.dumps(return_dict)
+        return HttpResponse(json, mimetype="application/json")
+    else:
+        return redirect('home')
 
 #eof
