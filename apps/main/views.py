1687bb95d5ff2bd0e3f737cce1ade99a4a8f28de
P. Christeas <xrg@linux.gr>
2013-02-18 11:54:18 +0200

    home.html: the department selection shall be a combo
    We have too many roles per user, a <ul> is not usable any more.

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,12 +1,14 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+import logging
 
 def home(request):
+    logger = logging.getLogger('permissions')
     try:
         user_profile = request.user.get_profile()
     except Exception, e:
-        #print "exc:", e
+        logger.warning("Cannot get profile:", exc_info=True)
         pass
     current_user_role = None
     try:
@@ -19,7 +21,7 @@ def home(request):
         else:
             user_roles = []
     except Exception, e:
-        # print "roles exc:", e
+        logger.warning("Cannot prepare user role:", exc_info=True)
         user_roles = []
 
     return render_to_response('home.html', {'user_profile': user_profile, 
