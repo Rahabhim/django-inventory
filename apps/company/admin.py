711030f72bae74c01987943638d800eebb7497cd
P. Christeas <xrg@linux.gr>
2013-02-28 09:57:14 +0200

    company/admin: use ajax select to avoid huge combos

--- apps/company/admin.py
+++ apps/company/admin.py
@@ -1,8 +1,14 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
 from models import DepartmentType, Department
+from ajax_select import make_ajax_form
+from ajax_select.admin import AjaxSelectAdmin
+
+class DepartmentAdmin(AjaxSelectAdmin):
+    form = make_ajax_form(Department, { 'parent':'department', 
+                        'serviced_by': 'department', 'merge': 'department'})
 
 admin.site.register(DepartmentType)
-admin.site.register(Department)
+admin.site.register(Department, DepartmentAdmin)
 
 #eof
