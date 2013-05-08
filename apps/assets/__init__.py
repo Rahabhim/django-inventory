5a6c3f67cbb6b936991643927b1c253a2afd603a
P. Christeas <xrg@linux.gr>
2013-05-08 17:39:54 +0300

    assets: hide "editing" options for assets we don't own
    The views do check themselves if the user has access to these
    assets, but the sidebar menu didn't.
    
    Ticket: #51

--- apps/assets/__init__.py
+++ apps/assets/__init__.py
@@ -6,6 +6,9 @@ from models import State, Item, ItemGroup
 import models
 
 
+def asset_is_ours(obj, context):
+    return Item.objects.by_request(context['request']).filter(id=obj.id).exists()
+
 state_list = {'text':_('assets states'), 'view':'state_list', 'famfam':'error_go'}
 state_create = {'text':_('create new asset state'), 'view':'state_create', 'famfam':'error_add'}
 state_edit = {'text':_(u'edit asset state'), 'view':'state_update', 'args':'object.id', 'famfam':'error'}
@@ -14,12 +17,12 @@ state_delete = {'text':_(u'delete asset state'), 'view':'state_delete', 'args':'
 
 asset_list = {'text':_('list all assets'), 'view':'item_list', 'famfam':'computer'}
 # asset_create = {'text':_('create new asset'), 'view':'item_create', 'famfam':'computer_add'}
-asset_edit = {'text':_(u'edit serials'), 'view':'item_update', 'args':'object.id', 'famfam':'computer_edit'}
+asset_edit = {'text':_(u'edit serials'), 'view':'item_update', 'args':'object.id', 'famfam':'computer_edit', 'condition':  asset_is_ours}
 # asset_delete = {'text':_(u'delete'), 'view':'item_delete', 'args':'object.id', 'famfam':'computer_delete'}
-asset_photos = {'text':_(u'add / remove photos'), 'view':'item_photos', 'args':'object.id', 'famfam':'picture_edit'}
+asset_photos = {'text':_(u'add / remove photos'), 'view':'item_photos', 'args':'object.id', 'famfam':'picture_edit', 'condition':  asset_is_ours}
 asset_template = {'text':_(u'template'), 'view':'template_view', 'args':'object.item_template.id', 'famfam':'page_go'}
-asset_history = {'text':_(u'trace'), 'view':'item_history_view', 'args':'object.id', 'famfam':'book_open'}
-asset_printout = {'text':_(u'printout'), 'view':'asset_printout', 'args':'object.id', 'famfam':'print'}
+asset_history = {'text':_(u'trace'), 'view':'item_history_view', 'args':'object.id', 'famfam':'book_open', 'condition':  asset_is_ours}
+asset_printout = {'text':_(u'printout'), 'view':'asset_printout', 'args':'object.id', 'famfam':'print', 'condition':  asset_is_ours}
 
 
 group_list = {'text':_(u'list all groups'), 'view':'group_list', 'famfam':'chart_pie'}
@@ -52,7 +55,7 @@ register_menu([
         'famfam':'computer', 'position':2},
     ])
 
-asset_list_printout = {'text':_('print assets list'), 'view':'asset_list_printout2', 'famfam':'printer'}
+asset_list_printout = {'text':_('print assets list'), 'view':'asset_list_printout2', 'famfam':'printer' }
 register_links(['item_list',], [asset_list_printout,], menu_name='sidebar')
 
 register_links(['department_assets',], [ {'text':_('print assets list'), \
