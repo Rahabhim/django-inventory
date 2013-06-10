c4f8ec01fca1e7a6e2af0c5c7075571baff5a788
P. Christeas <xrg@linux.gr>
2013-06-10 13:35:23 +0300

    common/admin: make sure Department is initialized before Location

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from models import Location, LocationTemplate, Partner, Supplier, Sequence
 from ajax_select import make_ajax_form
 from ajax_select.admin import AjaxSelectAdmin
+from company.models import Department # needed to initialize models
+__hush = [ Department, ]
 
 class LocationAdmin(AjaxSelectAdmin):
     form = make_ajax_form(Location, dict(department='department'))
