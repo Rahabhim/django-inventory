6271a526b79968a765bcf60113f326efdcf7a1e8
P. Christeas <xrg@linux.gr>
2013-06-14 15:38:33 +0300

    dhelp: show sub-topics under each topic

--- apps/dhelp/views.py
+++ apps/dhelp/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from models import HelpTopic
 
+sub_topic_modes = {'app': [ ('model', _("Models")), ('view', _("Views"))],
+            'model': [('field', _("Fields")),],
+            }
+
 def help_display_view(request, object_id):
     topic = get_object_or_404(HelpTopic, pk=object_id)
     user_can_edit = False
@@ -14,8 +18,15 @@ def help_display_view(request, object_id):
     else:
         raise PermissionDenied
 
+    sub_topics = []
+    for smode, title in sub_topic_modes.get(topic.mode, []):
+        sts = HelpTopic.objects.by_request(request)\
+                    .filter(mode=smode, tkey__startswith=topic.tkey +'.')
+        if sts.exists():
+            sub_topics.append((title, sts))
+
     return render_to_response('help_topic_display.html',
-            {'topic': topic, 'can_edit': user_can_edit },
+            {'topic': topic, 'can_edit': user_can_edit, 'sub_topics': sub_topics },
             context_instance=RequestContext(request))
 
 def help_index_view(request, key=None, mode=None):
