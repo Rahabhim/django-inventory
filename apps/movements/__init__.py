f0c2f44b382354fe55dd6b740b4807a27302a402
P. Christeas <xrg@linux.gr>
2013-08-08 18:02:15 +0300

    movements: fix link from PO to mass-insert mode wizard

--- apps/movements/__init__.py
+++ apps/movements/__init__.py
@@ -70,7 +70,7 @@ purchase_order_update = {'text':_('edit order'), 'view':'purchase_order_update',
 
 purchase_order_updwiz = {'text':_('edit order items'), 'view':'purchaseorder_wizard_update', 'args':'object.id', 'famfam':'pencil', 'condition': (iz_open, iz_single_dept,  can_edit, has_no_pending_inventories)  }
 purchase_order_updwiz_mass = {'text':_('edit order items (mass)'), 
-            'view':'purchaseorder_wizard_update', 'args':'object.id', 'famfam':'pencil',
+            'view':'purchaseorder_wizard_update_mass', 'args':'object.id', 'famfam':'pencil',
             'condition': (iz_open, lambda o,c: o.department is None, can_do_mass_po, can_edit, has_no_pending_inventories)  }
 purchase_order_delete = {'text':_('delete order'), 'view':'purchase_order_delete', 'args':'object.id', 'famfam':'cart_delete', 'condition': (iz_open_or_rej, can_delete)  }
 purchase_order_close = {'text':_('close order'), 'view':'purchase_order_close', 'args':'object.id', 'famfam':'cross'}
