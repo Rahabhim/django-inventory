a65242724334717b7ae980e9b54e6f91aa2f68a6
P. Christeas <xrg@linux.gr>
2014-01-16 10:14:54 +0200

    po_wizard: fix usage of immutable dict at step 3
    http://stackoverflow.com/questions/12611345/django-why-is-the-request-post-object-immutable

--- apps/movements/views_po_wizard.py
+++ apps/movements/views_po_wizard.py
@@ -121,9 +121,11 @@ class PO_Step3(WizardForm):
         if data and self.data.get(it_pref, {}):
             item = ItemTemplate.objects.get(pk=self.data[it_pref])
             category = item.category
-            if data.get(pa_pref, {}).get('from_category', False) != category:
+            if data and data.get(pa_pref, {}).get('from_category', False) \
+                    and data[pa_pref]['from_category'] != category:
                 # reset that and any attributes set by previous category
-                data[pa_pref] = {'from_category': category}
+                self.data = self.data.copy()
+                self.data[pa_pref] = {'from_category': category}
         elif data and self.data.get(pa_pref, {}).get('from_category', False):
             # Set the manufacturer according to the existing products of `from_category` ,
             # in descending popularity order
