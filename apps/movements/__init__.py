dc287e01b709ee75aa9d4e5b34f3d2de04786154
P. Christeas <xrg@linux.gr>
2014-02-28 15:22:06 +0200

    movements: have the list of movements as a submenu of "assets"

--- apps/movements/__init__.py
+++ apps/movements/__init__.py
@@ -168,7 +168,6 @@ movement_delete = {'text':_('delete pending movement'), 'view':'movement_delete'
             'args':'object.id', 'famfam':'basket_delete', \
             'condition': (iz_open_or_rej, can_delete_movement) }
 
-# register_submenu('menu_assets', .. )
 action_destroy = dict(text=_(u'Destroy assets'), view='destroy_items', famfam='computer_delete', condition= (can_add(Movement), has_no_pending_inventories))
 action_lose = dict(text=_(u'Lose assets'), view='lose_items', famfam='computer_error', condition= (can_add(Movement), has_no_pending_inventories))
 action_move = dict(text=_(u'Move assets'), view='move_items', famfam='computer_go', condition= (can_add(Movement), has_no_pending_inventories))
@@ -177,6 +176,10 @@ action_move_internal = dict(text=_(u'Move assets (internal)'), view='move_items_
 register_links( ['item_list'], [ action_destroy, action_lose, action_move, action_move_internal ], menu_name='sidebar')
 register_links(['home',], [action_destroy, action_lose, action_move, action_move_internal ], menu_name='start_actions')
 register_links(['home',], [purchase_order_wizard, purchase_order_wizard_mass ], menu_name='start_actions')
+
+movements_list = dict(text=_(u'Movements'), view='movements_list', famfam='computer_go')
+
+register_submenu('menu_assets', [movements_list] )
 
 register_links([('purchase_order_receive', Movement),], 
         [ {'text':_(u'details'), 'view':'movement_view', 'args':'object.id',
