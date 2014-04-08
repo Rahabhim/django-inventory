7680cae0ea00180790158ff02098faac15edc92f
P. Christeas <xrg@linux.gr>
2014-04-08 15:21:08 +0300

    movements: fix 6628b3170bf, order by name

--- apps/movements/weird_fields.py
+++ apps/movements/weird_fields.py
@@ -175,8 +175,7 @@ class IGW_Attribute(StrAndUnicode):
     @property
     def manufs(self):
         return Manufacturer.objects.filter(products__category=self._obj.category).\
-                        annotate(num_products=Count('products'))
-                        # .order_by('-num_products')
+                        annotate(num_products=Count('products')).order_by('name')
 
     @property
     def parts(self):
