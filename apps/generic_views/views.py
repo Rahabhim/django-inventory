60625b801f68f92e94e5776fa7c23e1e6d80582a
P. Christeas <xrg@linux.gr>
2014-05-03 08:36:51 +0300

    generic_views: remove debug print line

--- apps/generic_views/views.py
+++ apps/generic_views/views.py
@@ -793,7 +793,6 @@ class JSON_RPC_ResponseMixin(object):
         return self.get_json_response(self.convert_context_to_json(context))
 
     def get_json_response(self, content, **httpresponse_kwargs):
-        print "JSON:", content
         return HttpResponse(content, content_type='application/json', **httpresponse_kwargs)
 
     def convert_context_to_json(self, context):
