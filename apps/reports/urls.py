cd2b8a29ac88d00b639df07adb3de8a94c2e8589
P. Christeas <xrg@linux.gr>
2014-05-09 22:20:30 +0300

    reports: attributes, product fields that depend on category
    Implementation of widgets that depend on the value of a
    sibling field. Whenever the product category changes, we
    fetch the grammar for that particular category.
    Luckily, AngularJS does exactly what we need: adapts the
    other fields in real time, based on a watched variable.

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 urlpatterns = patterns('reports.views',
     url(r'^$', 'reports_app_view', (), name='reports_app_view'),
     url(r'^grammar/(?P<rep_type>\w+)$', 'reports_grammar_view', (), name='reports_grammar_view'),
+    url(r'^cat-grammar/(?P<cat_id>\d+)$', 'reports_cat_grammar_view', (), name='reports_cat_grammar_view'),
     url(r'^parts/params-(?P<part_id>\w+).html$', 'reports_parts_params_view', (), name='reports_parts_params_view'),
     )
 
