72333484f6c49ceddb51fb96b398f24115b299cd
P. Christeas <xrg@linux.gr>
2014-05-20 09:14:13 +0300

    reports: load/save/delete functionality for reports
    Settings (parameters and formatting) of reports all live inside
    Javascript, as a data structure. We can serialize them to JSON
    and send them to Django for storage.
    
    The only model needed is a flat table of "saved reports", with
    a data-structure field to save these arbitrary report settings.

--- apps/reports/__init__.py
+++ apps/reports/__init__.py
@@ -1,9 +1,16 @@
 # -*- encoding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
-from common.api import register_links, register_menu
+from common.api import register_links, register_menu, can_add
+
+from models import SavedReport
+
+report_create = {'text':_('create new report'), 'view':'reports_app_view',
+        'famfam':'report_add', 'condition': can_add(SavedReport)}
+
+register_links(['reports_list_view',], [report_create], menu_name='sidebar')
 
 register_menu([
-    {'text':_('Reports'), 'view':'reports_app_view',
+    {'text':_('Reports'), 'view':'reports_list_view',
         'famfam':'report', 'position':20,
     },
 ])
