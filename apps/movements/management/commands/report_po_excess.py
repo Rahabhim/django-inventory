dc4ab57a2345ad2d9f09a0645e3f4859f5d9e5bd
P. Christeas <xrg@linux.gr>
2014-06-11 20:15:23 +0300

    movements/mgmt: list excess items of PO check, in detail
    We need to examine the kind of items gone wrong in PO algorithm,
    so put them in a list and display them on the console.

--- apps/movements/management/commands/report_po_excess.py
+++ apps/movements/management/commands/report_po_excess.py
@@ -19,7 +19,9 @@ class Command(BaseCommand):
         (   make_option('-o', '--offset',
                 help='ID to compute from'),
             make_option('-l', '--limit',
-                help="Number of POs to compute")
+                help="Number of POs to compute"),
+            make_option('-d', '--detail', action="store_true", default=False,
+                help="List excess items, in detail"),
         )
 
     def handle(self, *args, **options):
@@ -29,12 +31,12 @@ class Command(BaseCommand):
         if args:
             pos = pos.filter(pk__in=map(int, args))
         elif options.get('offset'):
-            print "offset:", options['offset']
             pos = pos.filter(pk__gt=int(options['offset']))
 
         if options.get('limit'):
             pos = pos[:int(options['limit'])]
 
+        show_detail = options['detail']
         for po in pos:
             log.info("Processing purchase order: %d", po.id)
             try:
@@ -47,13 +49,24 @@ class Command(BaseCommand):
                                 all_ids.append(o.item_id)
 
                 num_excess = 0
+                excess_items = []
                 for move in po.movements.all():
                     for it in move.items.all():
                         if it.id not in all_ids:
                             num_excess += 1
+                            excess_items.append(it)
 
                 if num_excess:
                     print "PO #%d %s has %d excess items" % (po.id, po, num_excess)
+                    if show_detail:
+                        for poi in po.items.all():
+                            print "    %s x%d" %(poi, poi.qty)
+                            for boi in poi.bundled_items.all():
+                                print "        %s x%d" % (boi, boi.qty)
+                        if excess_items:
+                            print "Excess items:"
+                            for ei in excess_items:
+                                print "    %s #%d" % (ei, ei.id)
 
             except ValueError, ve:
                 log.warning("Cannot map items: %s", ve)
