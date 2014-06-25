ad1a5589754e8ecbd027228c6c309ede1acb10dc
P. Christeas <xrg@linux.gr>
2014-06-25 17:59:07 +0300

    movements/mgmt: report/analyze all open POs with that cmd.
    Previous version would only analyze POs selected by ID, now
    we can filter by state.

--- apps/movements/management/commands/report_analyze_po.py
+++ apps/movements/management/commands/report_analyze_po.py
@@ -16,15 +16,27 @@ from products.models import ItemTemplate
 class Command(BaseCommand):
     args = ''
     help = 'Analyzes items in PO order'
-    option_list = BaseCommand.option_list
+    option_list = BaseCommand.option_list \
+            +( make_option('-a', '--all', action="store_true", default=False,
+                help="Analyze all draft POs"),
+            make_option('-l', '--limit',
+                help="Number of POs to compute (in --all mode, only)"),
+            make_option('--short', action="store_true", default=False,
+                help="Short display, no analysis"),
+            )
 
     def handle(self, *args, **options):
         log = logging.getLogger('apps.movements.commands')
 
-        do_delete = False
+        all_mode = False
         pos = PurchaseOrder.objects.order_by('id')
         if args:
             pos = pos.filter(pk__in=map(int, args))
+        elif options.get('all', False):
+            pos = pos.filter(state__in=('draft', 'pending'))
+            if options.get('limit', False):
+                pos = pos[:options['limit']]
+            all_mode = True
         else:
             log.error("you must give some IDs to search for")
             return False
@@ -33,8 +45,13 @@ class Command(BaseCommand):
             log.info("Analyzing purchase order: %d", po.id)
             try:
                 mapped_items = po.map_items()
+                if all_mode and not po.map_has_left(mapped_items):
+                    continue
+                print "Purchase Order #%d: %s (by %s on %s)" %(po.id, po, po.create_user, po.department or '?')
                 all_ids = []
                 all_serials = []
+                if options.get('short', False):
+                    continue
                 for loc_kind, tdict in mapped_items.items():
                     print "Location:", loc_kind or '*'
                     for product, objs in tdict.items():
@@ -54,6 +71,7 @@ class Command(BaseCommand):
                             #if o.item_id:
                             #    all_ids.append(o.item_id)
 
+                print # empty line after each PO
             except ValueError, ve:
                 log.warning("Cannot map items for PO %#d: %s", po.id, ve)
 
