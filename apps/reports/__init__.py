2b1e449e6ba8b3fba79e37d74dea9114d73d3964
P. Christeas <xrg@linux.gr>
2014-07-15 11:13:49 +0300

    reports: by default, reports are read-only, for rendering
    Some users may be denied access to the reports AngularJS app.
    By default, clicking on a report from the list should lead to
    the "details" view, where the user can only call the report
    for rendering.

--- apps/reports/__init__.py
+++ apps/reports/__init__.py
@@ -1,13 +1,18 @@
 # -*- encoding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
-from common.api import register_links, register_menu, can_add
+from common.api import register_links, register_menu, can_add, can_edit
 
 from models import SavedReport
 
 report_create = {'text':_('create new report'), 'view':'reports_app_view',
         'famfam':'report_add', 'condition': can_add(SavedReport)}
 
+report_edit = {'text': _('edit report'),
+        'url_fn': lambda r: r.get_edit_url(), 'args': ['object'],
+        'famfam': 'pencil', 'condition': can_edit}
+
 register_links(['reports_list_view',], [report_create], menu_name='sidebar')
+register_links(SavedReport, [report_edit])
 
 register_menu([
     {'text':_('Reports'), 'view':'reports_list_view',
