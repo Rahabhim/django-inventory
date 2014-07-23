6ead26a3e41fcc9f4ea43fb1bac008eef600a181
P. Christeas <xrg@linux.gr>
2014-07-23 18:36:04 +0300

    reports: allow staff users to edit reports, see the menu

--- apps/reports/__init__.py
+++ apps/reports/__init__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
-from common.api import register_links, register_menu, can_add, can_edit
+from common.api import register_links, register_menu, can_add, can_edit, user_is_staff
 
 from models import SavedReport
 
@@ -10,7 +10,7 @@ report_create = {'text':_('create new report'), 'view':'reports_app_view',
 report_edit = {'text': _('edit report'),
         'url_fn': lambda r: r.get_edit_url(), 'args': ['object'],
         'hide_text': True,
-        'famfam': 'pencil', 'condition': can_edit}
+        'famfam': 'pencil', 'condition': lambda o,c: can_edit(o,c) or user_is_staff(o,c)}
 
 register_links(['reports_list_view',], [report_create], menu_name='sidebar')
 register_links(SavedReport, [report_edit])
