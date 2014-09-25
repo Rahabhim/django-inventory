251ad2f39a5eb58069826c140af3b44b3eb1317b
P. Christeas <xrg@linux.gr>
2014-09-25 23:10:07 +0300

    reports: completely refactor the HTML results renderer
    Previous "straight" approach would totally mess with table
    cell counting, led to a garbled table.
    Re-write the algorithm, pre-parse *all* the data rows in a
    recursive Python loop, then calculate counts and "rowspans",
    store that in data structures and render accordingly.
    
    And, yes, we have missed that line border, so far.
    
    Obligatory xkcd ref:
        http://xkcd.com/1425/

--- apps/reports/templatetags/report_tags.py
+++ apps/reports/templatetags/report_tags.py
@@ -69,75 +69,26 @@ class GroupNode(Node):
         super(GroupNode, self).__init__()
         self.grp_list = grp_list
 
-    def _render_group(self, group_level, rows_filter, context):
-        if group_level + 1 >= len(context['groupped_results']):
-            return
-        group = context['groupped_results'][group_level+1]
-        context['has_next_results'] = bool( group_level+2 < len(context['groupped_results']))
+    def _render_group(self, cur_group, data, context):
         context.push()
-        context['cur_group'] = group
-        grp_fields = context['groupped_fields'].get(str(group_level), [])
-        context['cur_grp_fields'] = grp_fields
-        context['do_group_recurse'] = lambda: self._render_group_flat(group_level+1, context)
-        context['cur_results'] = filter(rows_filter, group['values'])
-        if grp_fields and grp_fields[0].get('group_mode',False) == 'left_col':
-            self._compute_right_cols(group_level, context)
+        context['cur_group'] = cur_group
+        for k, v in cur_group.items():
+            context[k] = v
+
+        if cur_group.get('sub_group'):
+            context['do_group_recurse'] = lambda: self._render_group_flat( cur_group['sub_group'], context)
         else:
-            context['right_fields'] = []
+            context['do_group_recurse'] = lambda: ''
+        context['cur_results'] = data
+        context['get_rowspan'] = lambda: context['cur_row'].get('_rowspan', 1)
+        context['has_sub_values'] = lambda: bool(context['cur_row'].get('_sub_values'))
         yield self.grp_list.render(context)
         context.pop()
         return
-        
-    def _compute_right_cols(self, group_level, context):
-        i = group_level + 1
-        results = context['groupped_results']
-        ret = []
-        last_factor = 1
-        while i < len(results)-1:
-            cur_results = results[i+1]
-            if cur_results and cur_results.get('group_by'):
-                cur_grp_fields = context['groupped_fields'].get(str(i), [{},])
-                if cur_grp_fields[0].get('group_mode') == 'table':
-                    l = cur_grp_fields[:]
-                    l.sort(key=lambda fc: fc.get('sequence'))
-                    l.append({'id': '_count', 'name': _("Count")})
-                    last_factor = 2
-                    ret += l
-                    break
-                elif cur_grp_fields[0].get('group_mode') == 'left_col':
-                    ret.append(cur_grp_fields[0])
-                else:
-                    ret.append({'id': '_count'})
-                    break
-            elif cur_results:
-                # detailed results
-                l = context['field_cols'][:]
-                # sort?
-                ret.extend(l)
-                break
-            i += 1
-        context['right_fields'] = ret
 
-        def get_rowspan():
-            # note that we're evaluating `context['cur_row']` at the time of
-            # this function call, meaning it will give a different count each time
-            nrf = self._get_rows_filter(context['cur_group']['group_by'], context['cur_row'])
-            o = group_level + 1
-            count_child_rows = 1
-            while o <= i and o < len(results)-1:
-                n = sum(map(nrf, results[o+1]['values']))
-                count_child_rows += n
-                if o == i and last_factor == 2:
-                    count_child_rows += n
-                o += 1
-            return count_child_rows
-
-        context['get_rowspan'] = get_rowspan
-
-    def _render_group_flat(self, group_level, context):
+    def _render_group_flat(self, cur_group, context):
         nodelist = NodeList()
-        nrf = self._get_rows_filter(context['cur_group']['group_by'], context['cur_row'])
-        for res in self._render_group(group_level, nrf, context):
+        for res in self._render_group(cur_group, context['cur_row'].get('_sub_values'), context):
             nodelist.append(res)
         return nodelist.render(context)
 
@@ -153,11 +104,112 @@ class GroupNode(Node):
             return True
         return rfilter
 
+    def _pre_calc_fields(self, context, group_level=0):
+        res = {}
+        results = context['groupped_results'][1:]
+        res = None
+        in_table = False
+        while results:
+            cur_results = results.pop()
+            if cur_results and cur_results.get('group_by'):
+                level = cur_results['group_level']
+                cur_grp_fields = context['groupped_fields'].get(str(level-1), [{},])
+                g = {'group_level': level,
+                    'group_by': cur_results['group_by'],
+                    'group_mode': cur_grp_fields[0].get('group_mode', 'row'),
+                    'sub_group': res,
+                    'in_table': in_table,
+                    }
+                if g['group_mode'] == 'table':
+                    l = cur_grp_fields[:]
+                    l.sort(key=lambda fc: fc.get('sequence'))
+                    l.append({'id': '_count', 'name': _("Count"), 'widget': ''})
+                    g['field_cols'] = [{'id': f['id'], 'widget': f['widget']} for f in l]
+                    g['render_head'] = [{'id': f['id'], 'name': f['name']} for f in l]
+                    in_table = True
+
+                elif g['group_mode'] == 'row':
+                    l = cur_grp_fields[:]
+                    l.sort(key=lambda fc: fc.get('sequence'))
+                    # l.append({'id': '_count', 'name': False, 'widget': ''})
+                    g['field_cols'] = [{'id': f['id'], 'widget': f['widget'], 'name': f['name']} for f in l]
+                    g['render_head'] = [{'name': '-'}]
+                    in_table = False
+
+                elif g['group_mode'] == 'left_col':
+                    col0 = {'id': False, 'name': cur_grp_fields[0]['name']}
+                    if res:
+                        g['render_head'] = res.pop('render_head')
+                        res['render_head'] = False
+                    else:
+                        g['render_head'] = []
+                    g['render_head'].insert(0, col0)
+                    l = cur_grp_fields[:]
+                    l.sort(key=lambda fc: fc.get('sequence'))
+                    g['field_cols'] = []
+                    for f in l:
+                        c = {'id': f['id'], 'widget': f['widget']}
+                        if f is not cur_grp_fields[0]:
+                            c['name'] = f['name']
+                        g['field_cols'].append(c)
+                    in_table = True
+                else:
+                    raise ValueError("unknown group mode: %s" % g['group_mode'])
+            elif cur_results:
+                # detailed results
+                fc = context['field_cols']
+                g = {'group_level': cur_results['group_level'],
+                    'field_cols': [{'id': f['id'], 'widget': f['widget']} for f in fc],
+                    'render_head': [{'id': f['id'], 'name': f['name']} for f in fc],
+                    'group_by': False, 'group_mode': False }
+            # unshift stack:
+            res = g
+
+        return res
+
+    def _pre_calc_data(self, context, fields):
+        """ Convert set of flat results lists to a nested tree
+
+            @param fields nested fields definitions, result of `_pre_calc_fields()`
+        """
+        rowspan_getter = lambda srow: srow.get('_rowspan', 1)
+
+        if fields['group_by'] and fields['sub_group']:
+            ivals = []
+            group_by = fields['group_by']
+            for row in context['groupped_results'][fields['group_level']].pop('values'):
+                ivals.append((self._get_rows_filter(group_by, row), row))
+                row['_sub_values'] = [] # after rows filter!
+
+            for sub_row in self._pre_calc_data(context, fields['sub_group']):
+                for fn, row in ivals:
+                    if fn(sub_row):
+                        row['_sub_values'].append(sub_row)
+                        break
+                else:
+                    raise RuntimeError("value not belonging to any group: %r" % sub_row)
+
+            if fields['group_mode'] == 'table':
+                for fn, row in ivals:
+                    if row['_sub_values']:
+                        row['_rowspan'] = 2
+            elif fields['group_mode'] == 'left_col':
+                for fn, row in ivals:
+                    row['_rowspan'] = sum(map(rowspan_getter, row['_sub_values'])) + 1
+
+            return [row for fn, row in ivals]
+        else:
+            # no more levels to process
+            return context['groupped_results'][fields['group_level']].pop('values')
+
     def render(self, context):
         nodelist = NodeList()
         context.push()
-        for res in self._render_group(0, lambda row: True, context):
-            nodelist.append(res)
+        cur_group = self._pre_calc_fields(context)
+        data = self._pre_calc_data(context, cur_group)
+
+        for res in self._render_group(cur_group, data, context):
+           nodelist.append(res)
         context.pop()
         return nodelist.render(context)
 
